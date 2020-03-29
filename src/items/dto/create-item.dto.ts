62a3de9c7c4fe1fc088b19fa44de7b1b6756743b
varunkverma <varunkverma1996@gmail.com>
2020-03-29 17:14:41 +0530

    added controllers, services and dto, interface and schema for Item and connection to mongodb

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,5 @@
 export class CreateItemDto {
   readonly name: string;
   readonly description: string;
-  readonly qty: number;
+  readonly quantity: number;
 }
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,37 +8,41 @@ import {
   Param,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-import { Request, Response } from 'express';
+
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   //   @Get()
   //   findAll(@Req() req: Request, @Res() res: Response): Response {
   //     return res.send('Hello World');
   //   }
 
   @Get()
-  findAll(): string {
-    return 'Get All items';
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id): string {
-    return `Item: ${id}`;
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
-    return `Name: ${createItemDto.name} qty: ${createItemDto.qty} Desc: ${createItemDto.description}`;
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
   }
 
   @Delete(':id')
-  delete(@Param('id') id): string {
-    return `Delete item: ${id}`;
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
   }
 
   @Put(':id')
-  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
-    return `Update ${id} - Name: ${updateItemDto.name}`;
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
   }
 }
